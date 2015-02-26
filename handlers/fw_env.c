cf998892f4ef278f39911270464952e63a3f1704
Jörg Krause <joerg.krause@embedded.rocks>
2015-02-26 20:24:10 +0100

    Use a global define for uboot-scripts max line length

--- handlers/fw_env.c
+++ handlers/fw_env.c
@@ -439,7 +439,7 @@ int fw_setenv(int argc, char *argv[])
 int fw_parse_script(char *fname)
 {
 	FILE *fp;
-	char dump[1024];	/* Maximum line length in the file */
+	char dump[MAX_UBOOT_SCRIPT_LINE_LENGTH];	/* Maximum line length in the file */
 	char *name;
 	char *val;
 	int lineno = 0;
